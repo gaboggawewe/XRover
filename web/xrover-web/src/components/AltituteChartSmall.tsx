9345bab244aedf53d2e681fa129d002f39a20e1b
Alejandro <alexnachovc12@gmail.com>
2024-11-27 13:11:22 -0600

    make small charts for the pressure sensors, to have them in all sensors

--- web/xrover-web/src/components/AltituteChartSmall.tsx
+++ web/xrover-web/src/components/AltituteChartSmall.tsx
@@ -1,19 +1,19 @@
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-const PressureChartSmall = ({ data }) => {
+const AltitudeChartSmall = ({ data }) => {
   return (
     <div className="w-full h-full">
-      <h2>Pressure Over Time</h2>
+      <h2>Altitude Over Time</h2>
         <LineChart width={600} height={300} data={data}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="timestamp" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="pressure" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="altitude" stroke="#ffc658" />
         </LineChart>
     </div>
   );
 };
 
-export default PressureChartSmall;
+export default AltitudeChartSmall;
--- web/xrover-web/src/components/TemperatureChartSmall.tsx
+++ web/xrover-web/src/components/TemperatureChartSmall.tsx
@@ -1,19 +1,19 @@
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-const PressureChartSmall = ({ data }) => {
+const TemperatureChartSmall = ({ data }) => {
   return (
     <div className="w-full h-full">
-      <h2>Pressure Over Time</h2>
+      <h2>Temperature Over Time</h2>
         <LineChart width={600} height={300} data={data}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="timestamp" />
           <YAxis />
           <Tooltip />
           <Legend />
-          <Line type="monotone" dataKey="pressure" stroke="#82ca9d" />
+          <Line type="monotone" dataKey="temperature" stroke="#8884d8" />
         </LineChart>
     </div>
   );
 };
 
-export default PressureChartSmall;
+export default TemperatureChartSmall;
